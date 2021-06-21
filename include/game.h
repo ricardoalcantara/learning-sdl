4c94b60a65bf50657ebef856c9c96e74750f9e54
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2021-06-21 01:38:08 -0300

    Compilando windows e linux

---
+++ include/game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Game {
+public: 
+	Game(); // constructor
+	~Game(); // destroyer
+
+	void init(const char* title, int x, int y, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clear();
+
+	bool running() {
+		return isRunning;
+	}
+
+private:
+	int ticker = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+
+
+};
+
+#endif 
--- src/texturemanager.cpp
+++ src/texturemanager.cpp
@@ -2,6 +2,11 @@
 
 SDL_Texture* TextureManager::LoadTexture(const char* fileName, SDL_Renderer* ren) {
 	SDL_Surface* tempSurface = IMG_Load(fileName);
+
+	if(!tempSurface) {
+		printf("IMG_Load: %s\n", IMG_GetError());
+	}
+
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tempSurface);
 	SDL_FreeSurface(tempSurface);
 
